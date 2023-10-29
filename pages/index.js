e83568c4a31b7777855ee9f2c5c42e60ce4d0125
Saumya Pathak <saumyasays2019@gmail.com>
2023-10-30 02:30:01 +0530

    added codebase

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import SearchBar from '../components/SearchBar';
+import NftCard from '../components/NftCard';
+
+const Home = () => {
+  const [nftData, setNftData] = useState(null);
+
+  const handleSearch = async (address, contractAddress) => {
+    setNftData(null);
+    try {
+      const response = await axios.get(`/api/chainbaseApi?address=${address}&contract_address=${contractAddress}`);
+      setNftData(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <img src="/logo.png" alt="Chainbase Logo" style={{ marginTop: '20px', width: '300px', height: '50px' }} />
+      <SearchBar onSearch={handleSearch} />
+
+      {nftData && nftData.length > 0 ? (
+        <div>
+          <h2>NFTs for Address: {nftData[0].owner}</h2>
+          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+            {nftData.map((nft) => (
+              <NftCard key={`${nft.contract_address}-${nft.token_id}`} nft={nft} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
